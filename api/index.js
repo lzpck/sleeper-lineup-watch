ceeaea4d4404d74525c6b5326a04ba3dbfc2665b
Leandro Zepechouka <leandro.mdrs@outlook.com>
2025-09-14 14:25:58 -0300

    feat: Configura projeto para deploy na Vercel e corrige FUNCTION_INVOCATION_FAILED

---
+++ api/index.js
@@ -0,0 +1,27 @@
+// Função serverless para Vercel
+const { app } = require('../dist/app');
+
+/**
+ * Handler para serverless function na Vercel
+ * @param {import('http').IncomingMessage} req - Request object
+ * @param {import('http').ServerResponse} res - Response object
+ */
+module.exports = (req, res) => {
+  // Configura headers CORS para todas as respostas
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  // Delega para a aplicação Express
+  return app(req, res);
+};
